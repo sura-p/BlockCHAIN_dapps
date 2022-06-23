0943537cd048a9ee085f3432df3242b9b0b20d03
Suraj Prajapati <31574333+sura-p@users.noreply.github.com>
2022-06-23 09:26:49 +0530

    Add files via upload

--- 2_supplychain/migrations/1_initial_migration.js
+++ 2_supplychain/migrations/1_initial_migration.js
@@ -1,5 +1,10 @@
-const Migrations = artifacts.require("Migrations");
 
-module.exports = function (deployer) {
-  deployer.deploy(Migrations);
+var ProductManagement = artifacts.require('Supply_Chain');
+var ChangeOwnership = artifacts.require('ownwership');
+
+module.exports = function(deployer) {
+  deployer.deploy(ProductManagement)
+  .then(function(){
+    return deployer.deploy(ChangeOwnership,ProductManagement.address);
+  })
 };
