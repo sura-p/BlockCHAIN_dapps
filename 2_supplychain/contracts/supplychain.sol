d65298fa91a3683211a5c5b53df86461051aaa51
Suraj Prajapati <31574333+sura-p@users.noreply.github.com>
2022-06-24 17:50:58 +0530

    Add files via upload

--- 2_supplychain/contracts/supplychain.sol
+++ 2_supplychain/contracts/supplychain.sol
@@ -2,39 +2,57 @@
 pragma solidity >=0.5.22 <=0.9.0;
 
 contract Supply_Chain {
-    enum State {
-        MANUFACTURE,
-        SUPPLIER,
-        CUSTOMER
+    struct Order {
+        address manufactureradd;
+        string part_type;
+        string deliverOn;
+        string orderdate;
     }
 
+
     struct Part {
-        address manufacturer;
-        string serial_number;
+        address supplier;
+        string manufacturedate;
         string part_type;
-        string creation_date;
+        string serialno;
+        uint256 price;
     }
     struct Product {
         address manufacturer;
         string serial_number;
         string product_type;
-        string creation_date;
+        string manufacturedate;
         bytes32[6] parts;
+        uint256 price;
     }
+    struct part_recepit{
+        bytes32 orderhash;
+        uint partPrice;
 
+    }
+    mapping(bytes32=>part_recepit) public part_detail;
+    mapping(bytes32 => Order) public order;
     mapping(bytes32 => Product) public product;
-    mapping (bytes32 => Part) public part;
+    mapping(bytes32 => Part) public part;
+event paymentDone(address reciver,address sender,uint256 amount,bytes32);
 
+modifier onlymanufacturer(bytes32 hash){
+    
+    require(order[hash].manufactureradd==msg.sender,"not the right manufacturer");
+    _;
+}
+
+//making hash
     function INFO_AND_HASH(
         address _address,
-        string memory serial_no,
+        string memory orderdate,
         string memory part_type,
-        string memory creation_date
+        string memory deliverOn
     ) private pure returns (bytes32) {
         bytes20 b_a1 = bytes20(_address);
-        bytes memory b_s1 = bytes(serial_no);
+        bytes memory b_s1 = bytes(orderdate);
         bytes memory b_s2 = bytes(part_type);
-        bytes memory b_s3 = bytes(creation_date);
+        bytes memory b_s3 = bytes(deliverOn);
 
         string memory s_full = new string(
             b_a1.length + b_s1.length + b_s2.length + b_s3.length
@@ -56,69 +74,133 @@ contract Supply_Chain {
         }
         return keccak256(b_full);
     }
-
-    function make_part(
-        string memory serial_number,
+// creating order
+    function create_order(
         string memory part_type,
-        string memory creation_date
-    ) public returns (bytes32) {
-        State state;
-        state=State.SUPPLIER;
+        string memory deliverOn,
+        string memory orderdate
+    ) public  returns (bytes32) {
+
+        bytes32 order_hash = INFO_AND_HASH(
+            msg.sender,
+            part_type,
+            deliverOn,
+            orderdate
+        );
+        Order memory new_order = Order(
+            msg.sender,
+            part_type,
+            deliverOn,
+            orderdate
+        );
+
+        
+       order[order_hash] = new_order;
+        return order_hash;
+    }
+//accessing the order
+
+    function get_order(bytes32 orderhash) public view returns (Order memory) {
+        return order[orderhash];
+    }
+//making part after order recivied
+    function make_part(bytes32 orderhash, string memory manufacturedate ,string memory serialno ,uint256 price)
+        public
+        returns (bytes32 partHash)
+    {
         bytes32 part_hash = INFO_AND_HASH(
             msg.sender,
-            serial_number,
-            part_type,
-            creation_date
+            order[orderhash].part_type,
+            order[orderhash].deliverOn,
+            order[orderhash].orderdate
         );
         require(
-            part[part_hash].manufacturer == address(0),
+            part[part_hash].supplier == address(0),
             "product Already exists"
         );
         Part memory new_part = Part(
             msg.sender,
-            serial_number,
-            part_type,
-            creation_date
+            manufacturedate,
+            order[orderhash].part_type,
+            serialno,
+            price*10**18
         );
         part[part_hash] = new_part;
         return part_hash;
     }
+//after making part ,configuring part receipt
+        function partrecepit(bytes32 orderhash,bytes32 partHash)public returns(bytes32){
+            uint price = part[partHash].price;
+            part_recepit memory recepit = part_recepit(orderhash,price);
+            part_detail[orderhash]=recepit;
+            return orderhash;
+        }
+
+        function getpartrecepit(bytes32 orderhash)public view returns(part_recepit memory){
+            
+            return part_detail[orderhash];
+        }
+   //making payment to get part     
+        function getparts(address payable receiver,bytes32 orderh ) onlymanufacturer(orderh) payable external returns (bool) {
+        uint256 amount = msg.value;
+        require(part[orderh].price==amount,"supplier price not matched");
+        receiver.transfer(amount);  
+        emit paymentDone(receiver,msg.sender,msg.value,orderh);
+        return true;
+}
 
     function make_Product(
         string memory serialno,
         string memory product_type,
         string memory creation_date,
-        bytes32[6] memory parts_array
+        bytes32[6] memory parts_array,
+        uint price
     ) public returns (bytes32) {
-            State state;
-            state=State.MANUFACTURE;
-            uint  i ;
-        for (uint index = 0; index < parts_array.length; index++) {
-
-                require(part[parts_array[i]].manufacturer != address(0), "part not available");
+        for (uint256 index = 0; index < parts_array.length; index++) {
+            require(
+                part[parts_array[index]].supplier != address(0),
+                "part not available"
+            );
         }
-                bytes32 product_hash = INFO_AND_HASH(
+        bytes32 product_hash = INFO_AND_HASH(
             msg.sender,
             serialno,
             product_type,
             creation_date
+            
         );
 
-                Product memory new_product = Product(msg.sender ,serialno,product_type,creation_date,parts_array);
-                
-                product[product_hash]=new_product;
+        Product memory new_product = Product(
+            msg.sender,
+            serialno,
+            product_type,
+            creation_date,
+            parts_array,
+            price*10**18
+        );
 
-           return product_hash; 
-       
+        product[product_hash] = new_product;
+
+        return product_hash;
     }
 
-       function getParts(bytes32 product_hash) public view  returns (bytes32[6] memory){
-       
-        require(product[product_hash].manufacturer != address(0), "Product inexistent");
+    function getProductParts(bytes32 product_hash)
+        public
+        view
+        returns (bytes32[6] memory)
+    {
+        require(
+            product[product_hash].manufacturer != address(0),
+            "Product inexistent"
+        );
         return product[product_hash].parts;
     }
 
-
-
-
+    function getPartInfo(bytes32 parthash)
+        public
+        view
+        returns (Part memory detail)
+    {
+        return part[parthash];
+    }
 }
